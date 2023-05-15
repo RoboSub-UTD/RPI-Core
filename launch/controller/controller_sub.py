5ad20157de05396a3545ed7ce09e7de5e3ebbed8
Shoaib Huq <huq.shoaib@gmail.com>
2023-05-15 03:18:49 -0500

    working controller code

--- launch/controller/controller_sub.py
+++ launch/controller/controller_sub.py
@@ -6,29 +6,29 @@ import serial
 
 class ControllerSub(Node):
     def __init__(self):
-        self.get_logger().debug('Starting initialization..')
         super().__init__('controller_sub')
+        self.get_logger().info('Starting initialization..')
         self.subscription = self.create_subscription(
             Joy,
             '/joy',
             self.listener_callback,
             10)
-        self.serial_port = serial.Serial('/dev/ttyAMA0', 9600)
+        #self.serial_port = serial.Serial('/dev/ttyAMA0', 9600)
 
-        self.get_logger().debug('Reached the end of initialization..')
+        self.get_logger().info('Reached the end of initialization..')
 
     def listener_callback(self, msg):
-        self.get_logger().info('Received joystick data: axes=%s, buttons=%s' % (msg.axes, msg.buttons))
+        #self.get_logger().info('Received joystick data: axes=%s, buttons=%s' % (msg.axes, msg.buttons))
         try:
-            self.serial_port.write(str(msg.axes).encode() + b'\n')
-            self.serial_port.write(str(msg.buttons).encode() + b'\n')
+            #self.serial_port.write(str(msg.axes).encode() + b'\n')
+            #self.serial_port.write(str(msg.buttons).encode() + b'\n')
 
             self.get_logger().info(str(msg.axes).encode() + b'\n')
             self.get_logger().info(str(msg.buttons).encode() + b'\n')
         except:
             self.get_logger().info('Input being received...')
 
-        self.get_logger().debug('Reached the end of the callback..')
+        #self.get_logger().info('Reached the end of the callback..')
 
 
 def main(args=None):
